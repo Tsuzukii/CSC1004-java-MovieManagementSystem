dfedd07fa6deb701af4bc53e7ad39d9b55429383
Tsuzukii <122090897@link.cuhk.edu.cn>
2023-02-20 22:09:23 +0800

    2/20

--- src/main/java/com/example/moviemanagement/loginViewController.java
+++ src/main/java/com/example/moviemanagement/loginViewController.java
@@ -7,14 +7,13 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
+import java.sql.*;
 
 import java.io.IOException;
 
-public class sceneController {
+public class loginViewController {
+    //JavaFX variables
     @FXML
     private TextField usernameField;
 
@@ -30,10 +29,29 @@ public class sceneController {
     @FXML
     private Button myLogin;
 
+    @FXML
+    private Button trueRegister;
+
+    @FXML
+    private TextField usernameFieldRegister;
+
+    @FXML
+    private PasswordField passwordFieldRegister;
+
+    @FXML
+    private TextField realNameRegister;
+
+    @FXML
+    private TextField telNumberRegister;
+
+
+
+
     private Stage stage;
     private Scene scene;
     private Parent root;
 
+    //switch scenes from login page to register scene when clicking the "register" button
     public void switchToRegister(ActionEvent register) throws IOException{
         root = FXMLLoader.load(getClass().getResource("registerView.fxml"));
         stage = (Stage)((Node)register.getSource()).getScene().getWindow();
@@ -43,6 +61,7 @@ public class sceneController {
         System.out.println("Switching to Register");
     }
 
+    //switch scenes from register page to login scene when clicking the "back" button
     public void switchToLogin(ActionEvent register) throws IOException{
         root = FXMLLoader.load(getClass().getResource("loginView.fxml"));
         stage = (Stage)((Node)register.getSource()).getScene().getWindow();
@@ -50,6 +69,22 @@ public class sceneController {
         stage.setScene(scene);
         stage.show();
         System.out.println("Switching to Login");
+    }
+
+    //deal with login verification
+    private void loginVerification() throws ClassNotFoundException, SQLException {
+        System.out.println("Start dealing with login");
+        //load mysql driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        //database information
+        String mysqlurl = "jdbc:mysql://localhost:3306/MovieManagementSystem";
+        //connect to database
+        Connection myConnection = DriverManager.getConnection(mysqlurl, "root", "18721376230");
+        if (myConnection != null) {
+            System.out.println("Database Connected Successfully");
+        }
+
+
     }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.moviemanagement {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.moviemanagement to javafx.fxml;
