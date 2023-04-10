f9a282c8e349fafb6540995340bc6e30938a9fb4
Tsuzukii <122090897@link.cuhk.edu.cn>
2023-04-10 21:41:33 +0800

    04/10

--- src/main/java/com/example/moviemanagement/adminUserViewController.java
+++ src/main/java/com/example/moviemanagement/adminUserViewController.java
@@ -1,10 +1,14 @@
 package com.example.moviemanagement;
 
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +22,16 @@ public class adminUserViewController {
         movie.add("The Shawshank Redemption");
         movie.add("Forrest Gump");
         movie.add("Leon");
+    }
+
+    public void switchToLogin (ActionEvent register) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("loginView.fxml"));
+        stage = (Stage) ((Node) register.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        stage.centerOnScreen();
+        System.out.println("Switching to CommonUser");
     }
 
 
--- src/main/java/com/example/moviemanagement/recommendGPTController.java
+++ src/main/java/com/example/moviemanagement/recommendGPTController.java
@@ -37,47 +37,7 @@ public class recommendGPTController {
         addMsg(text, true);
         addMsg(text, false);
     }
-/*
-    <AnchorPane prefHeight="472.0" prefWidth="683.0" xmlns="http://javafx.com/javafx/19" xmlns:fx="http://javafx.com/fxml/1" fx:controller="com.example.moviemanagement.recommendGPTController">
-   <children>
-      <ScrollPane layoutX="2.0" prefHeight="399.0" prefWidth="682.0">
-         <content>
-            <VBox fx:id="msgBox" prefHeight="396.0" prefWidth="680.0">
-               <children>
-                  <AnchorPane prefHeight="151.0" prefWidth="680.0">
-                     <children>
-                        <ImageView fitHeight="64.0" fitWidth="63.0" layoutX="570.0" layoutY="42.0" pickOnBounds="true" preserveRatio="true" />
-                        <Label layoutX="181.0" layoutY="41.0" prefHeight="68.0" prefWidth="344.0" style="-fx-border-color: black;" text="Label&#10;qq&#10;" />
-                     </children>
-                  </AnchorPane>
-                  <AnchorPane layoutX="10.0" layoutY="10.0" prefHeight="168.0" prefWidth="680.0">
-                     <children>
-                        <ImageView fitHeight="64.0" fitWidth="63.0" layoutX="34.0" layoutY="64.0" pickOnBounds="true" preserveRatio="true">
-                           <image>
-                              <Image url="@../../../images/openai.jpg" />
-                           </image>
-                        </ImageView>
-                        <Label layoutX="129.0" layoutY="62.0" prefHeight="68.0" prefWidth="351.0" style="-fx-border-color: black;" text="Label&#10;qq&#10;" />
-                     </children>
-                  </AnchorPane>
-               </children>
-            </VBox>
-         </content>
-      </ScrollPane>
-      <JFXTextArea fx:id="infoInputText" focusColor="#78c2c4" layoutX="125.0" layoutY="418.0" prefHeight="37.0" prefWidth="441.0" />
-      <RXFillButton fx:id="myEixt" layoutX="12.0" layoutY="411.0" prefHeight="48.0" prefWidth="93.0" text="Exit">
-         <font>
-            <Font size="28.0" />
-         </font>
-      </RXFillButton>
-      <RXFillButton fx:id="mySend" fillType="RIGHT_TO_LEFT" layoutX="577.0" layoutY="410.0" onAction="#sendMsg" prefHeight="48.0" prefWidth="93.0" text="Send">
-         <font>
-            <Font size="28.0" />
-         </font>
-      </RXFillButton>
-   </children>
-</AnchorPane>
-   */
+
     private void addMsg(String msg, boolean judgeUser){
        AnchorPane pane = new AnchorPane();
        pane.setPrefSize(683.0, 472.0);
@@ -89,7 +49,7 @@ public class recommendGPTController {
        if (judgeUser){
           myImage.setLayoutX(570.0);
           myImage.setLayoutY(42.0);
-          //myImage.setImage(new Image("file:"));
+          myImage.setImage(new Image("file:/Users/felixyan/IdeaProjects/CSC1004-java-MovieManagementSystem2/src/main/resources/images/user.png"));
        }
        else {
           myImage.setLayoutX(34);
@@ -99,7 +59,7 @@ public class recommendGPTController {
        Label message = new Label(msg);
        message.setStyle("-fx-border-color: black");
        message.setPrefSize(351, 68);
-       message.setLayoutX(129.0);
+       message.setLayoutX(135.0);
        message.setLayoutY(62);
        pane.getChildren().addAll(myImage, message);
        msgBox.getChildren().add(pane);
